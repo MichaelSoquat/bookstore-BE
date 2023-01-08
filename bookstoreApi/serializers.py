0b927166708fac86c2dccb419d09f2e504bf89ad
MichaelSoquat <michael.soquat@gmx.de>
2023-01-08 16:10:35 +0100

    changed book model

--- bookstoreApi/serializers.py
+++ bookstoreApi/serializers.py
@@ -6,7 +6,7 @@ from .models import Book, User
 class BookSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Book
-        fields = ['id','title', 'description', 'author', 'genre', 'buyed_at', 'price', 'is_read', 'rating', 'notice']
+        fields = ['id','title', 'description', 'author', 'genre', 'buyed_at', 'normal_price', 'buy_place', 'payed', 'is_read', 'rating', 'notice']
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
